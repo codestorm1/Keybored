33e7b2881a38e78ed39e9ce8b6b6c375273771c1
Bryan Green <twistedogre@gmail.com>
2022-08-28 16:41:42 -0700

    Created KeyboredUI and firmware projects.  Associated UI as a dependency of firmware

---
+++ keybored_firmware/lib/keybored_firmware.ex
@@ -0,0 +1,18 @@
+defmodule KeyboredFirmware do
+  @moduledoc """
+  Documentation for KeyboredFirmware.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KeyboredFirmware.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ keybored_firmware/test/keybored_firmware_test.exs
@@ -0,0 +1,8 @@
+defmodule KeyboredFirmwareTest do
+  use ExUnit.Case
+  doctest KeyboredFirmware
+
+  test "greets the world" do
+    assert KeyboredFirmware.hello() == :world
+  end
+end
